a0e5df308ae5c668168dfca154a6e54b666358ea
顾翔 <c0710204@gmail.com>
2014-09-21 17:03:40 +0800

    moti_index

--- index.php
+++ index.php
@@ -9,21 +9,26 @@
 	<script src="http://cdn.bootcss.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
-	<body>
-	<div class="jumbotron">
-	<div class='container'>
+	<body >
+	<img src='img/logo.jpg'  style='z-index:-1; position:fixed;width:100%'></img>
+	<div class="jumbotron" style="background-color:rgba(0,0,0,0)">
+	<div class='container' style="color:rgb(0, 255, 235);">
 	<h1>源程序2092</h1>
-	<div class='row'>
-<img src='img/logo.jpg'  class='img-responsive col-md-2'></img>
-<div  class='col-md-8'><p>自从设定好的核战又过去了多年，破灭的钟声再次响起组织的游戏又一次粉墨登场注定消亡的子嗣与走向永恒的贵族间的战场这一次选择在虚拟的星空钢铁，血肉，皆在火焰中蒸腾；深爱，仇恨，同在风中飞舞巨大机械的撞击中，人类的脚步偏离了组织计划一颗丢错的骰子，一个无限的未知</p>
-<a class='btn btn-primary'  target="_blank" href="http://book.zongheng.com/book/119951.html">阅读小说</a>
-<a class='btn btn-default' href="yh.php">随机数发生器</a>
-<a class='btn btn-info' href="zyjl6.php">废墟争夺战</a>
-<a class='btn btn-info' href="zyjl5.php">殖民卫星突入战</a>
-<a class='btn btn-info' href="zyjl4.php">ms战</a>
-<a class='btn btn-info' href="zyjl3.php">间谍战</a>
-<a class='btn btn-info' href="zyjl2.php">潛艇战</a>
-<a class='btn btn-info' href="">量子深渊</a>
+	<div class='row' >
+<!-- <img src='img/logo.jpg'  class='img-responsive col-md-2'> </img>-->
+<div  class='col-md-5'><p >自从设定好的核战又过去了多年，破灭的钟声再次响起组织的游戏又一次粉墨登场注定消亡的子嗣与走向永恒的贵族间的战场这一次选择在虚拟的星空钢铁，血肉，皆在火焰中蒸腾；深爱，仇恨，同在风中飞舞巨大机械的撞击中，人类的脚步偏离了组织计划一颗丢错的骰子，一个无限的未知</p></div><div  class='col-md-7'>
+<div class="row">
+<a class='btn btn-primary col-md-4'  target="_blank" href="http://book.zongheng.com/book/119951.html">阅读小说</a>
+<a class='btn btn-default col-md-4' href="yh.php">随机数发生器</a>
+<a class='btn btn-info col-md-4' href="">量子深渊</a>
+</div>
+<div class="row">
+<a class='btn btn-info col-md-3' href="zyjl6.php">废墟争夺战</a>
+<a class='btn btn-info col-md-3' href="zyjl5.php">殖民卫星突入战</a>
+<a class='btn btn-info col-md-2' href="zyjl4.php">ms战</a>
+<a class='btn btn-info col-md-2' href="zyjl3.php">间谍战</a>
+<a class='btn btn-info col-md-2' href="zyjl2.php">潛艇战</a>
+</div>
 
 
 </div>
